38434ea1073b415cb30c1e0c683522cb5e22a52e
Moritz <mo.hesche@gmail.com>
2022-03-02 21:36:56 +0100

    added main test file

---
+++ main_test.py
@@ -0,0 +1,37 @@
+from src.logic_model import TetrisGame
+from src.ui import AsciisDisplay
+
+
+if __name__ == "__main__":
+    from pynput import keyboard
+
+    display = AsciisDisplay()
+    game = TetrisGame(display, 15, 7)
+
+    keymap = {
+        "s": game.start,
+        "S": game.start,
+        "p": game.pause,
+        "P": game.pause,
+        "r": game.reset,
+        "R": game.reset,
+        keyboard.Key.left: game.move_left,
+        keyboard.Key.right: game.move_right,
+        keyboard.Key.up: game.rotate_right,
+        keyboard.Key.down: game.rotate_left,
+        keyboard.Key.shift: game.drop,
+        keyboard.Key.shift_r: game.drop,
+    }
+
+    def on_press(key):
+        try:
+            identifier = key.char
+        except AttributeError:
+            identifier = key
+
+        action = keymap.get(identifier)
+        if action is not None:
+            action()
+
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
