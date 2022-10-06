b1a7680e29a47ec1b0b960fdf5719ceecf6af3f9
Moritz <mo.hesche@gmail.com>
2022-10-06 23:31:31 +0200

    more merges

--- CADtris.py
+++ CADtris.py
@@ -1,75 +1,54 @@
-import traceback
+# pylint:disable=logging-fstring-interpolation
+
 import logging
-import os
-from datetime import datetime
+import traceback
+from pathlib import Path
 
-import adsk.core
-
-app = adsk.core.Application.cast(adsk.core.Application.get())
-ui = app.userInterface
-
-try:
-    from .apper.utilities import create_default_logger
-    from .apper.apper import FusionApp
-    from .apper.apper.UiElements import Workspace, Toolbar, Tab, Panel  # pylint: disable = unused-import
-    from .apper.apper.UiElements import Dropdown, CommandControl, SplitButtonControl  # pylint: disable = unused-import
-    from .apper.apper.UiElements import ButtonDefinition, CheckBoxDefinition, ListDefinition  # pylint: disable = unused-import
-    from .apper.apper.Enums import Workspaces, Toolbars, Tabs, Panels  # pylint: disable = unused-import
-
-    from .commands.CADtrisCommandDir import CADtrisCommand  # cookiecutter CADtrisCommand (+ Dir)
-
-    logfile = os.path.join(
-        os.path.abspath(os.path.dirname(__file__)), 'logs',
-        datetime.now().strftime(
-            '%Y-%m-%d_%H-%M-%S_CADtris_log.txt'))  # cookiecutter CADtris
-    logger = create_default_logger(
-        name='CADtris_logger',  # cookiecutter CADtris
-        handlers=[logging.StreamHandler(),
-                  logging.FileHandler(logfile)],
-        level=logging.ERROR)
-
-    cadtris_addin = FusionApp(  # cookiecutter cadtris
-        name='CADtrisAddin',  # cookiecutter CADtris
-        debug_to_ui=True,
-        logger=logger,
-    )
-
-    cadtris_addin.add_command(  # cookiecutter cadtris
-        command_class=CADtrisCommand,  # cookiecutter CADtris
-        positions=[[
-            Workspace(),
-            Tab(),
-            Panel('Fun'),  # cookiecutter Addins
-            CommandControl(),
-            ButtonDefinition(
-                'CADtris',  # cookiecutter CADtris
-                # cookiecutter remove resources and tooltip
-                resources=os.path.join(cadtris_addin.resources, 'icon'),
-                tooltip='Have fun with playing CADtris!')
-        ]])
-
-except:
-    app = adsk.core.Application.get()
-    ui = app.userInterface
-    message = 'Initialization Failed: {0}'.format(traceback.format_exc())
-    logger.error(message)
-    if ui:
-        ui.messageBox(message)
+from .addin.libs.fusion_addin_framework import fusion_addin_framework as faf
+from .addin import config
+from .addin.commands.CADTris import CADTrisCommand
 
 
-def run(context):  # pylint: disable = unused-argument
-    """[summary]
+def run(context):  # pylint:disable=unused-argument
+    try:
+        # setup logging
+        if config.LOGGING_ENABLED:
+            Path(config.LOGGING_FOLDER).mkdir(parents=True, exist_ok=True)
+            faf.utils.create_logger(
+                __name__,  # also applies to faf since its a submodule
+                [
+                    logging.StreamHandler(),
+                    # faf.utils.TextPaletteLoggingHandler(),
+                    logging.handlers.TimedRotatingFileHandler(
+                        config.LOGGING_FOLDER / config.LOGFILE_BASENAME,
+                        when=config.LOGGING_ROTATE_WHEN,
+                        interval=config.LOGGING_ROTATE_INTERVAL,
+                        backupCount=config.LOGGING_ROTATE_COUNT,
+                    ),
+                ],
+            )
+            logging.getLogger(__name__).info(f"Logging to {config.LOGGING_FOLDER}")
 
-    Args:
-        context ([type]): [description]
-    """
-    cadtris_addin.run_app()  # cookiecutter cadtris
+        # create the top level addin instance
+        addin = faf.FusionAddin()
+
+        # create the command
+        CADTrisCommand(addin)
+
+    except:  # pylint:disable=bare-except
+        msg = "Failed:\n{}".format(traceback.format_exc())
+        ui = faf.utils.AppObjects().userInterface
+        if ui:
+            ui.messageBox(msg)
+        print(msg)
 
 
-def stop(context):  # pylint: disable = unused-argument
-    """[summary]
-
-    Args:
-        context ([type]): [description]
-    """
-    cadtris_addin.stop_app()  # cookiecutter cadtris
+def stop(context):  # pylint:disable=unused-argument
+    try:
+        faf.stop()
+    except:  # pylint:disable=bare-except
+        msg = "Failed:\n{}".format(traceback.format_exc())
+        ui = faf.utils.AppObjects().userInterface
+        if ui:
+            ui.messageBox(msg)
+        print(msg)
